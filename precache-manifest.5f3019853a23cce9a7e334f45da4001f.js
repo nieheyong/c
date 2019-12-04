dbd2febadb93abc58f07a8063a4ba0266d5a90d4
Heyong Nie <niehey@163.com>
2019-12-04 19:19:21 +0800

    c

---
+++ precache-manifest.5f3019853a23cce9a7e334f45da4001f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72b9891462baedfc6b9",
+    "url": "css/app.49418b63.css"
+  },
+  {
+    "cdn": true,
+    "url": "https://cdn.jsdelivr.net/npm/core-js-bundle@3.4.3/minified.js"
+  },
+  {
+    "cdn": true,
+    "url": "https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css"
+  },
+  {
+    "cdn": true,
+    "url": "https://cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js"
+  },
+  {
+    "cdn": true,
+    "url": "https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.runtime.min.js"
+  },
+  {
+    "cdn": true,
+    "url": "https://cdn.jsdelivr.net/npm/vuex@3.1.2/dist/vuex.min.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6c06afc29b713d2ea306ff2398772eb0",
+    "url": "index.html"
+  },
+  {
+    "revision": "3cad24e8ac652e300014",
+    "url": "js/about.d26a54f5.js"
+  },
+  {
+    "revision": "f72b9891462baedfc6b9",
+    "url": "js/app.945fc224.js"
+  },
+  {
+    "revision": "67e0500b0042bbc15b7286e15e7ee4a1",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,21 @@
-importScripts("precache-manifest.09b27cb63dd32e4636fa1e4c7e9691db.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.5f3019853a23cce9a7e334f45da4001f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({ prefix: 'cdn-gitpage-test' })
 
 workbox.core.skipWaiting()
-
 workbox.core.clientsClaim()
 
-const cdnFiles = [
-  'https://cdn.jsdelivr.net/npm/core-js-bundle@3.4.3/minified.js',
-  'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.runtime.min.js',
-  'https://cdn.jsdelivr.net/npm/vuex@3.1.2/dist/vuex.min.js',
-  'https://cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js'
-].map(url => ({ url }))
+const cdnPath = 'https://cdn.jsdelivr.net/gh/nieheyong/c@gh-pages'
 
-const cdnPath = 'https://cdn.jsdelivr.net/gh/nieheyong/c@gh-pages/'
-
-const precache = self.__precacheManifest.map(asset => {
+const tryUseCdn = asset => {
   const hasHash = /\.[0-f]+\./
-  if (hasHash.test(asset.url)) asset.url = cdnPath + asset.url
+  if (hasHash.test(asset.url) && !asset.cdn) asset.url = cdnPath + asset.url
   return asset
-})
+}
 
-self.__precacheManifest = cdnFiles.concat(precache)
+self.__precacheManifest = []
+  .concat(self.__precacheManifest || [])
+  .map(tryUseCdn)
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
